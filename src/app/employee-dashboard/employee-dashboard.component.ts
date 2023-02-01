b1188f5f21082dc05f6d0533d0686590c5f39acd
Khalifa <khalifa.bihi351@campus.vinci.ac.ma>
2023-02-01 20:31:16 +0100

    dashboard

---
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-dashboard',
+  templateUrl: './employee-dashboard.component.html',
+  styleUrls: ['./employee-dashboard.component.css']
+})
+export class EmployeeDashboardComponent implements OnInit {
+
+  employees: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
